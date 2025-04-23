6a944ff0ec9e821519a2cdc6e6ae30e33d24e0f1
devillers <davidevillers@gmail>
2025-04-23 17:06:33 +0200

    wp3

--- app/components/ProfilesGrid.js
+++ app/components/ProfilesGrid.js
@@ -1,9 +1,11 @@
 import ProfileCard from "./ProfileCard";
 
 export default function ProfilesGrid({ profiles }) {
+  const safeProfiles = Array.isArray(profiles) ? profiles : [];
+
   return (
     <div className="max-w-3xl grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 gap-6 p-4 mx-auto">
-      {profiles.map((profile) => {
+      {safeProfiles.map((profile) => {
         const image = profile._embedded?.['wp:featuredmedia']?.[0]?.source_url || '';
         const meta = profile.meta || {};
         const {
