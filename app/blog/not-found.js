4f563a8ac6118ad47134e5fa1867984097fa47b9
devillers <davidevillers@gmail>
2025-04-24 20:12:05 +0200

    blog

--- app/blog/not-found.js
+++ app/blog/not-found.js
@@ -18,6 +18,7 @@ export default function NotFound() {
         <p className="text-md sm:text-xl md:text-2xl mb-8 font-thin">
           Vous semblez suivre le mauvais chemin...
         </p>
+        <div className="absolute inset-0 bg-radial-[at_25%_25%] from-white to-zinc-900 to-75% z-20"  />
         <a
           href="/blog"
            className="inline-flex items-center justify-center px-4 h-[30px] font-thin mt-4 border border-white text-sm uppercase text-white rounded-full transition-all duration-200 hover:bg-gray-900/60 hover:text-white active:scale-95"
--- app/repertoire/not-found.js
+++ app/repertoire/not-found.js
@@ -19,7 +19,7 @@
           <p className="text-lg sm:text-xl md:text-2xl mb-8 font-thin z-20">
             Vous semblez suivre le mauvais chemin...
           </p>
-          <div className="absolute inset-0 bg-gradient-to-bl from-transparent to-black/60 z-20" />
+          <div className="absolute inset-0 bg-radial-[at_25%_25%] from-white to-zinc-900 to-75% z-20"  />
           <a
             href="/"
         className="inline-flex items-center justify-center px-4 h-[30px] font-thin mt-4 border border-white text-sm uppercase text-white rounded-full transition-all duration-200 hover:bg-gray-900/60 hover:text-white active:scale-95 z-30"
