25ea0218c7238d78dd0a8ed287099f2af95b1217
devillers <davidevillers@gmail>
2025-04-24 20:02:56 +0200

    404

--- app/not-found.js
+++ app/not-found.js
@@ -18,15 +18,16 @@ export default function NotFound() {
     >
       <div className="absolute inset-0 bg-black bg-opacity-30 z-0" />
       <div className="relative z-20 flex flex-col items-center justify-center h-full px-6">
-        <h1 className="text-6xl sm:text-7xl md:text-8xl font-thin mb-4">
+        <h1 className="text-6xl sm:text-7xl md:text-8xl font-thin mb-4 z-20">
           404
         </h1>
-        <p className="text-lg sm:text-xl md:text-2xl mb-8 font-thin">
+        <p className="text-lg sm:text-xl md:text-2xl mb-8 font-thin z-20">
           Vous semblez suivre le mauvais chemin...
         </p>
+        <div className="absolute inset-0 bg-gradient-to-bl from-transparent to-black/60 z-20" />
         <a
           href="/"
-      className="inline-flex items-center justify-center px-4 h-[30px] font-thin mt-4 border border-white text-sm uppercase text-white rounded-full transition-all duration-200 hover:bg-gray-900/60 hover:text-white active:scale-95"
+      className="inline-flex items-center justify-center px-4 h-[30px] font-thin mt-4 border border-white text-sm uppercase text-white rounded-full transition-all duration-200 hover:bg-gray-900/60 hover:text-white active:scale-95 z-10"
         >
           Retour à l'accueil
         </a>
