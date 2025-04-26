a4df9a98785bd76e298151f072881e6d04bb25c6
devillers <davidevillers@gmail>
2025-04-26 16:21:53 +0200

    footer

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 const Footer = () => {
   return (
-    <footer className=" py-8 mt-[100px] ">
+    <footer className=" py-8 mt-[20px] ">
       <div className="max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-4 gap-8 px-4">
         <div>
           <div className="flex items-center gap-2">
