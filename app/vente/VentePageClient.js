a1426c886a0cec3716bcb6e9bdcadec34bfe98dd
devillers <davidevillers@gmail>
2025-05-05 10:45:11 +0200

    vente

--- app/vente/VentePageClient.js
+++ app/vente/VentePageClient.js
@@ -121,23 +121,23 @@ export default function VentePageClient() {
       </div>
 
       {/* Filtres et liste */}
-      <div className="max-w-6xl mx-auto p-6">
+      <div className="max-w-6xl mx-auto p-2 ">
         {/* UI des filtres */}
-        <div className="mb-8 p-6 bg-white">
-          <div className="flex flex-wrap items-center gap-8">
+        <div className="mb-8  bg-[#bd9254] p-3 rounded">
+          <div className="flex flex-wrap items-baseline gap-8">
             {/* Types de bien */}
             <div>
-              <p className="text-xs font-semibold text-[#bd9254] uppercase mb-2">
+              <p className="text-xs font-semibold text-white uppercase mb-2">
                 Type de bien
               </p>
-              <div className="flex flex-wrap gap-3">
+              <div className="flex flex-col gap-3">
                 {allTypes.map((cat) => {
                   const id = `type-${cat.slug}`;
                   return (
                     <label
                       key={cat.slug}
                       htmlFor={id}
-                      className="flex items-center gap-2 border border-gray-300 rounded px-4 py-1 text-[12px] cursor-pointer"
+                      className="flex uppercase text-[10px] font-light cursor-pointer"
                     >
                       <input
                         id={id}
@@ -147,7 +147,7 @@ export default function VentePageClient() {
                         onChange={() => toggleFilter("categories", cat.slug)}
                         className="accent-[#bd9254]"
                       />
-                      <span>{cat.name}</span>
+                      <span className="ml-1">{cat.name}</span>
                     </label>
                   );
                 })}
@@ -156,7 +156,7 @@ export default function VentePageClient() {
 
             {/* Villes */}
             <div>
-              <p className="text-xs font-semibold text-[#bd9254] uppercase mb-2">
+              <p className="text-xs font-semibold text-white uppercase mb-2">
                 Ville
               </p>
               <div className="flex flex-wrap gap-3">
@@ -167,7 +167,7 @@ export default function VentePageClient() {
                     <label
                       key={ville}
                       htmlFor={id}
-                      className="flex items-center gap-2 border border-gray-300 rounded px-4 py-1 text-[12px] cursor-pointer"
+                      className="flex uppercase text-[10px] font-light cursor-pointer"
                     >
                       <input
                         id={id}
@@ -175,9 +175,9 @@ export default function VentePageClient() {
                         type="checkbox"
                         checked={filters.villes.includes(ville)}
                         onChange={() => toggleFilter("villes", ville)}
-                        className="accent-[#bd9254]"
+                        className="accent-[#bd9254] "
                       />
-                      <span>{ville}</span>
+                     <span className="ml-1">{ville}</span>
                     </label>
                   );
                 })}
@@ -188,15 +188,15 @@ export default function VentePageClient() {
             <div>
               <label
                 htmlFor="prixMax"
-                className="text-xs font-semibold text-[#bd9254] uppercase mb-2 block"
+                className="text-xs font-semibold text-white uppercase mb-2 block"
               >
-                Prix maximum (€)
+                Prix max
               </label>
               <input
                 id="prixMax"
                 name="prixMax"
                 type="number"
-                className="border h-7 border-gray-300 text-xs rounded px-3 py-2 w-40"
+                className="border h-7 border-gray-300 text-xs rounded px-3 py-2 w-24 focus:outline-none focus:ring-0"
                 value={filters.prixMax}
                 onChange={(e) =>
                   setFilters({ ...filters, prixMax: e.target.value })
