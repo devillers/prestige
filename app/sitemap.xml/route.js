39a9d5313d194a06799f2ce21449d4bdcd32ff77
devillers <davidevillers@gmail>
2025-06-25 20:57:59 +0200

    gfg

--- app/sitemap.xml/route.js
+++ app/sitemap.xml/route.js
@@ -1,17 +1,16 @@
+//app/sitemap.xml
+
 export async function GET() {
-    const baseUrl = process.env.SITE_URL;
-  
-    const sitemapIndex = `<?xml version="1.0" encoding="UTF-8"?>
-    <sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-      <sitemap><loc>${baseUrl}/sitemap-static.xml</loc></sitemap>
-      <sitemap><loc>${baseUrl}/sitemap-blog.xml</loc></sitemap>
-      <sitemap><loc>${baseUrl}/sitemap-vente.xml</loc></sitemap>
-      <sitemap><loc>${baseUrl}/sitemap-repertoire.xml</loc></sitemap>
-      <sitemap><loc>${baseUrl}/sitemap-seminaires.xml</loc></sitemap>
-    </sitemapindex>`;
-  
-    return new Response(sitemapIndex, {
-      headers: { 'Content-Type': 'application/xml' }
-    });
-  }
-  
+  const baseUrl = process.env.SITE_URL;
+  const sitemapIndex = `<?xml version="1.0" encoding="UTF-8"?>
+  <sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    <sitemap><loc>${baseUrl}/sitemap-static.xml</loc></sitemap>
+    <sitemap><loc>${baseUrl}/sitemap-blog.xml</loc></sitemap>
+    <sitemap><loc>${baseUrl}/sitemap-vente.xml</loc></sitemap>
+    <sitemap><loc>${baseUrl}/sitemap-repertoire.xml</loc></sitemap>
+    <sitemap><loc>${baseUrl}/sitemap-seminaires.xml</loc></sitemap>
+  </sitemapindex>`;
+  return new Response(sitemapIndex, {
+    headers: { 'Content-Type': 'application/xml' }
+  });
+}
